b8fc233b54e2b74110b23e115076cad580a93d7e
Ramant <ramant@inqui-lab.org>
2024-05-03 15:40:43 +0530

    first draft for atl 2.0

---
+++ src/configs/wildcardRoutes.config.ts
@@ -0,0 +1,14 @@
+export const wildcardRoutes = [
+    "/",
+    "/api/v1/docs",
+    "/api/v1/healthcheck",
+    "/assets/*",
+    "/posters/*",
+    "/images/*",
+    "/assets/defaults/*",
+    "/assets/courses/*",
+    "/assets/reflectiveQuiz/*",
+    "/script",
+    "/otpScript",
+    "/api/v1/admins/login"
+];
