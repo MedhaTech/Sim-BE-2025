ac5d821817665b48598c9124c3fbd4b16b453f83
MedhaTech <159229048+MedhaTech@users.noreply.github.com>
2024-05-07 09:20:50 +0530

    Update app_stop.sh

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -3,8 +3,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 export HOME="/home/ubuntu/"
-export PM2_HOME=/home/ubuntu/.pm2
-cd /home/ubuntu/aim_be
-pm2 stop all
-pm2 delete all
-pm2 save -f
+export PM2_HOME=/home/ec2-user/.pm2
+sudo pm2 stop all
+sudo pm2 delete all
+sudo pm2 save -f
