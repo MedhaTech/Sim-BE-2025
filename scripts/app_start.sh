b8b787c4a9202086880fc0f2a0c3d05c44ebe9c7
MedhaTech <159229048+MedhaTech@users.noreply.github.com>
2024-05-09 12:03:00 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ubuntu/"
+export HOME="/home/ec2-user/"
 export PM2_HOME=/home/ec2-user/.pm2
 sudo npm run start:prod
 sudo pm2 save --force
