df4e6368f891b291a4778b8c7cb12c254801c97e
Ramant <ramant@inqui-lab.org>
2024-05-27 10:09:03 +0530

    add org and mentor api's

---
+++ src/models/mentor.model.ts
@@ -0,0 +1,130 @@
+import { DataTypes, Model, Attributes, InferAttributes, InferCreationAttributes, CreationOptional } from 'sequelize';
+import bcrypt from 'bcrypt';
+import { constents } from '../configs/constents.config';
+import db from '../utils/dbconnection.util';
+import { baseConfig } from '../configs/base.config';
+import { user } from './user.model';
+import { organization } from './organization.model';
+//import { student } from './student.model';
+
+
+export class mentor extends Model<InferAttributes<mentor>, InferCreationAttributes<mentor>> {
+    declare mentor_id: CreationOptional<number>;
+    declare user_id: number;
+    declare reg_status: number;
+    declare organization_code: string;
+    declare full_name: string;
+    declare mobile: string;
+    declare status: Enumerator;
+    declare created_by: number;
+    declare created_at: Date;
+    declare updated_by: number;
+    declare updated_at: Date;
+    declare title: string;
+    declare gender: string;
+    declare whatapp_mobile: string;
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+}
+
+mentor.init(
+    {
+        mentor_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        reg_status: {
+            type: DataTypes.ENUM(...Object.values(constents.res_status.list)),
+            defaultValue: constents.res_status.default,
+            allowNull: true
+        },
+        organization_code: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+       
+        full_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mobile: {
+            type: DataTypes.STRING,
+            unique: true
+        },
+        otp: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        status: {
+            type: DataTypes.ENUM(...Object.values(constents.common_status_flags.list)),
+            defaultValue: constents.common_status_flags.default
+        },
+        created_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+        },
+        updated_by: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            defaultValue: null
+        },
+        updated_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: DataTypes.NOW,
+            onUpdate: new Date().toLocaleString()
+        },
+        title: {
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        gender:{
+            type:DataTypes.STRING,
+            allowNull: false
+        },
+        whatapp_mobile:{
+            type:DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        sequelize: db,
+        tableName: 'mentors',
+        timestamps: true,
+        updatedAt: 'updated_at',
+        createdAt: 'created_at',
+        hooks: {
+            beforeCreate: async (user: any) => {
+                if (user.password) {
+                    user.password = await bcrypt.hashSync(user.password, process.env.SALT || baseConfig.SALT);
+                }
+            },
+            beforeUpdate: async (user) => {
+                if (user.password) {
+                    user.password = await bcrypt.hashSync(user.password, process.env.SALT || baseConfig.SALT);
+                }
+            }
+        }
+    }
+);
+
+mentor.belongsTo(user, { foreignKey: 'user_id', constraints: false, scope: { role: 'MENTOR' } });
+user.hasOne(mentor, { foreignKey: 'user_id', constraints: false });
+mentor.belongsTo(organization, { targetKey: 'organization_code', foreignKey: 'organization_code', constraints: false });
+organization.hasOne(mentor, { sourceKey: 'organization_code', foreignKey: 'organization_code', constraints: false });
+//mentor.belongsTo(student, { targetKey: 'team_id', foreignKey: 'organization_code', constraints: false });
+//student.hasOne(mentor, { sourceKey: 'team_id', foreignKey: 'organization_code', constraints: false });
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -13,6 +13,8 @@ import { constents } from '../configs/constents.config';
 import AWS from 'aws-sdk';
 import { user } from '../models/user.model';
 import { admin } from '../models/admin.model';
+import { organization } from '../models/organization.model';
+import { mentor } from '../models/mentor.model';
 export default class authService {
     crudService: CRUDService = new CRUDService;
     private otp = '112233';
@@ -223,5 +225,282 @@ export default class authService {
             return result;
         }
     }
+    /**
+     * find organization details using organization code and attach mentor details
+     * @param organization_code String
+     * @returns object
+     */
+    async checkOrgDetails(organization_code: any) {
+        try {
+            const org = await this.crudService.findOne(organization, {
+                where: {
+                    organization_code: organization_code,
+                    status: {
+                        [Op.or]: ['ACTIVE', 'NEW']
+                    }
+                },
+                include: {
+                    model: mentor,
+                    attributes: [
+                        "mentor_id",
+                        'user_id',
+                        'full_name',
+                        'mobile',
+                        'whatapp_mobile',
+                        'gender',
+                        'title'
+                    ],
+                    include: {
+                        model: user,
+                        attributes: [
+                            'username'
+                        ]
+                    }
+                }
+            })
+            return org;
+        } catch (error) {
+            return error;
+        }
+    }
+    /**
+     * registers the mentor
+     * @param requestBody object
+     * @returns Object
+     */
+    async mentorRegister(requestBody: any) {
+        let response: any;
+        try {
+            const user_data = await this.crudService.findOne(user, { where: { username: requestBody.username } });
+            if (user_data) {
+                throw badRequest('Email');
+            } else {
+                // const mentor_data = await this.crudService.findOne(mentor, { where: { mobile: requestBody.mobile } })
+                // if (mentor_data) {
+                //     throw badRequest('Mobile')
+                // } else {
+                    let createUserAccount = await this.crudService.create(user, requestBody);
+                    let conditions = { ...requestBody, user_id: createUserAccount.dataValues.user_id };
+                    let createMentorAccount = await this.crudService.create(mentor, conditions);
+                    createMentorAccount.dataValues['username'] = createUserAccount.dataValues.username;
+                    createMentorAccount.dataValues['user_id'] = createUserAccount.dataValues.user_id;
+                    response = createMentorAccount;
+                    return response;
+                }
+            // }
+        } catch (error) {
+            return error;
+        }
+    }
    
+    async mobileotp(requestBody: any) {
+        let result: any = {};
+        try {
+            const user_data = await this.crudService.findOne(user, { where: { username: requestBody.username } });
+            if (user_data) {
+                throw badRequest('Email');
+            }
+            else{
+                const otp = await this.triggerEmail(requestBody.username,1,'no');
+            if (otp instanceof Error) {
+                throw otp;
+            }
+            const hashedPassword = await this.encryptGlobal(JSON.stringify(otp.otp));
+            result.data = hashedPassword;
+            return result;
+            }
+        } catch (error) {
+            result['error'] = error;
+            return result;
+        }
+    }
+    /**
+     * find the user details and trigger OTP, update the password
+     * @param requestBody Object
+     * @param responseBody Object
+     * @returns Object
+     */
+    async triggerEmail(email: any,id:any, fulldata:any) {
+        const result: any = {}
+        const otp: any = Math.random().toFixed(6).substr(-6);
+        const verifyOtpdata = `<body style="border: solid;margin-right: 15%;margin-left: 15%; ">
+        <img src="https://aim-email-images.s3.ap-south-1.amazonaws.com/ATL-Marathon-Banner-1000X450px.jpg" alt="header" style="width: 100%;" />
+        <div style="padding: 1% 5%;">
+        <h3>Dear Guide Teacher,</h3>
+        
+        <p>Your One-Time Password (OTP) to register yourself as a guide teacher in ATL Marathon 23-24 is <b>${otp}</b></p>
+        
+        <p>We appreciate for your interest in inspiring students to solve problems with simplified design thinking process as a method to innovate through this program.</p>
+        <p>
+        <strong>
+        Regards,<br> ATL Marathon
+        </strong>
+        </div></body>`
+        const forgotPassData = `
+        <body style="border: solid;margin-right: 15%;margin-left: 15%; ">
+        <img src="https://aim-email-images.s3.ap-south-1.amazonaws.com/ATL-Marathon-Banner-1000X450px.jpg" alt="header" style="width: 100%;" />
+        <div style="padding: 1% 5%;">
+        <h3>Dear Guide Teacher,</h3>
+        <p>Your temporary password to login to ATL Marathon platform is <b>${otp}.</b></p>
+        <p>Change your password as per your preference after you login with temporary password.</p>
+        <p><strong>Link: https://atl.unisolve.org</strong></p>
+        <p>
+        <strong>
+        Regards,<br> ATL Marathon
+        </strong>
+        </p>
+        </div></body>`
+        const verifyOtpSubject =`OTP to register on AIM Platfrom`
+        const forgotPassSubjec =`Temporary Password to Login into AIM Platfrom`
+        const fullSubjec = `Welcome! Your AIM Registration was successful. Check out your login details`
+        AWS.config.update({
+            region: 'ap-south-1',
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        });
+        let params = {
+            Destination: { /* required */
+                CcAddresses: [
+                ],
+                ToAddresses: [
+                    email
+                ]
+            },
+            Message: { /* required */
+                Body: { /* required */
+                    Html: {
+                        Charset: "UTF-8",
+                        Data: id === 1 ? verifyOtpdata : id === 3 ? forgotPassData : fulldata
+                    },
+                    Text: {
+                        Charset: "UTF-8",
+                        Data: "TEXT_FOR MAT_BODY"
+                    }
+                },
+                Subject: {
+                    Charset: 'UTF-8',
+                    Data: id === 1 ? verifyOtpSubject : id === 3 ? forgotPassSubjec : fullSubjec
+                }
+            },
+            Source: "aim-no-reply@inqui-lab.org", /* required */
+            ReplyToAddresses: [],
+        };
+        try {
+            // Create the promise and SES service object
+            let sendPromise = new AWS.SES({ apiVersion: '2010-12-01' }).sendEmail(params).promise();
+            // Handle promise's fulfilled/rejected states
+            await sendPromise.then((data: any) => {
+                result['messageId'] = data.MessageId;
+                result['otp'] = otp;
+            }).catch((err: any) => {
+                throw err;
+            });
+            //result['otp'] = 112233;
+            return result;
+        } catch (error) {
+            return error;
+        }
+    }
+    /**
+     * Get the mentor details with the mobile number, trigger OTP and update the password
+     * @param requestBody 
+     * @returns 
+     */
+    async mentorResetPassword(requestBody: any) {
+        let result: any = {};
+        let mentor_res: any;
+        let mentor_id: any = requestBody.mentor_id;
+        let otp = requestBody.otp == undefined ? true : false;
+        let passwordNeedToBeUpdated: any = {};
+        try {
+            if (!otp) {
+                mentor_res = await this.crudService.findOne(user, {
+                    where: { username: requestBody.username }
+                });
+            } else {
+                mentor_res = await this.crudService.findOne(user, {
+                    where: { username: requestBody.email }
+                });
+            }
+            if (!mentor_res) {
+                result['error'] = speeches.USER_NOT_FOUND;
+                return result;
+            }
+            const user_data = await this.crudService.findOnePassword(user, {
+                where: { user_id: mentor_res.dataValues.user_id }
+            });
+            if (!otp) {
+                var pass = requestBody.username.trim();
+                var myArray = pass.split('@');
+                let word = myArray[0];
+                passwordNeedToBeUpdated['otp'] = word;
+                passwordNeedToBeUpdated["messageId"] = speeches.AWSMESSAGEID
+            } else {
+                const otpOBJ = await this.triggerEmail(requestBody.email,3,'no');
+                passwordNeedToBeUpdated['otp'] = otpOBJ.otp;
+                if (passwordNeedToBeUpdated instanceof Error) {
+                    throw passwordNeedToBeUpdated;
+                }
+            }
+            const findMentorDetailsAndUpdateOTP: any = await this.crudService.updateAndFind(mentor,
+                { otp: passwordNeedToBeUpdated.otp },
+                { where: { user_id: mentor_res.dataValues.user_id } }
+            );
+            passwordNeedToBeUpdated.otp = String(passwordNeedToBeUpdated.otp);
+            let hashString = await this.generateCryptEncryption(passwordNeedToBeUpdated.otp)
+            const user_res: any = await this.crudService.updateAndFind(user, {
+                password: await bcrypt.hashSync(hashString, process.env.SALT || baseConfig.SALT)
+            }, { where: { user_id: user_data.dataValues.user_id } })
+            result['data'] = {
+                username: user_res.dataValues.username,
+                user_id: user_res.dataValues.user_id,
+                awsMessageId: passwordNeedToBeUpdated.messageId
+            };
+            return result;
+        } catch (error) {
+            result['error'] = error;
+            return result;
+        }
+    }
+    async triggerWelcome(requestBody: any) {
+        let result: any = {};
+        try {
+            const {school_name,udise_code,atl_code,district,state,pin_code,email,mobile} = requestBody;
+            var pass = email.trim();
+            var myArray = pass.split('@');
+            let word = myArray[0];
+            const WelcomeTemp = `
+            <body style="border: solid;margin-right: 15%;margin-left: 15%; ">
+            <img src="https://aim-email-images.s3.ap-south-1.amazonaws.com/ATL-Marathon-Banner-1000X450px.jpg" alt="header" style="width: 100%;" />
+            <div style="padding: 1% 5%;">
+            <h3>Dear Guide Teacher,</h3>
+            <h4>Congratulations for successfully registering for ATL Marathon 23-24.</h4>
+            <p>Your schools has been successfully registered with the following details :
+            <br> School name: <strong> ${school_name}</strong> <br> UDISE CODE:<strong> ${udise_code}</strong>
+            <br> ATL CODE:<strong> ${atl_code}</strong>
+            <br> District:<strong> ${district}</strong>
+             <br> State:<strong> ${state}</strong>
+             <br> Pincode:<strong> ${pin_code}</strong>
+            </p>
+            <p> Below are your log-in details: </p>
+            <p> Login User ID: <strong> ${email} </strong>
+            <br>
+            Password: <strong>  ${word}
+            </strong> <br>
+            Mobile no: <strong> ${mobile} </strong>
+            <p>Please use your user id and password to login and proceed further.</p>
+            <p><strong>Link: https://atl.unisolve.org</strong></p>
+            <p><strong>Regards,<br> ATL Marathon</strong></p>
+            </div></body>`
+            const otp = await this.triggerEmail(email,2,WelcomeTemp);
+            if (otp instanceof Error) {
+                throw otp;
+            }
+            result.data = 'Email sent successfully'
+            return result;
+        } catch (error) {
+            result['error'] = error;
+            return result;
+        }
+    }
 }
