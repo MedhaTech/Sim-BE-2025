11ac8d1fe11aa64a1a02943eb7fc22d4b65280f8
Ramant <ramant@medhatech.in>
2025-03-10 11:38:18 +0530

    permisson for team role in youtube api

--- src/controllers/challenge_response.controller.ts
+++ src/controllers/challenge_response.controller.ts
@@ -1668,7 +1668,7 @@ export default class ChallengeResponsesController extends BaseController {
         }
     }
     protected async checkyoutubeurl(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
-        if (res.locals.role !== 'ADMIN' && res.locals.role !== 'STUDENT') {
+        if (res.locals.role !== 'ADMIN' && res.locals.role !== 'STUDENT' && res.locals.role !== 'TEAM') {
             return res.status(401).send(dispatcher(res, '', 'error', speeches.ROLE_ACCES_DECLINE, 401));
         }
         try {
