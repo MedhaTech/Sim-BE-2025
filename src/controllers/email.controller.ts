2eb2a5e34e8183616c189fc362bc16d427a6346f
Ramant <ramant@medhatech.in>
2025-03-21 18:37:51 +0530

    bulk email stats

--- src/controllers/email.controller.ts
+++ src/controllers/email.controller.ts
@@ -2,6 +2,11 @@
 import ValidationsHolder from "../validations/validationHolder";
 import BaseController from "./base.controller";
 import { emailSchema, emailUpdateSchema } from "../validations/email.validations";
+import { Request, Response, NextFunction } from 'express';
+import dispatcher from "../utils/dispatch.util";
+import { speeches } from "../configs/speeches.config";
+import db from "../utils/dbconnection.util"
+import { QueryTypes } from "sequelize";
 
 export default class EmailController extends BaseController {
 
@@ -11,9 +16,54 @@ export default class EmailController extends BaseController {
         this.path = '/emails';
     }
     protected initializeValidations(): void {
-        this.validations =  new ValidationsHolder(emailSchema,emailUpdateSchema);
+        this.validations = new ValidationsHolder(emailSchema, emailUpdateSchema);
     }
     protected initializeRoutes(): void {
+        this.router.get(`${this.path}/listEmailStats`, this.getlistEmailStats.bind(this));
+        this.router.get(`${this.path}/emailStats`, this.getEmailStats.bind(this));
         super.initializeRoutes();
+    }
+    private async getlistEmailStats(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        if (res.locals.role !== 'ADMIN') {
+            return res.status(401).send(dispatcher(res, '', 'error', speeches.ROLE_ACCES_DECLINE, 401));
+        }
+        try {
+            let newREQQuery: any = {}
+            if (req.query.Data) {
+                let newQuery: any = await this.authService.decryptGlobal(req.query.Data);
+                newREQQuery = JSON.parse(newQuery);
+            } else if (Object.keys(req.query).length !== 0) {
+                return res.status(400).send(dispatcher(res, '', 'error', 'Bad Request', 400));
+            }
+            const result = await db.query(`SELECT * FROM email_logs;`, { type: QueryTypes.SELECT });
+            return res.status(200).send(dispatcher(res, result, 'success'));
+        } catch (error) {
+            next(error);
+        }
+
+    }
+    private async getEmailStats(req: Request, res: Response, next: NextFunction): Promise<Response | void> {
+        if (res.locals.role !== 'ADMIN') {
+            return res.status(401).send(dispatcher(res, '', 'error', speeches.ROLE_ACCES_DECLINE, 401));
+        }
+        try {
+            const result = await db.query(`SELECT 
+    COUNT(CASE
+        WHEN status = 'Bounced' THEN 1
+    END) AS BouncedCount,
+    COUNT(CASE
+        WHEN status = 'Delivered' THEN 1
+    END) AS DeliveredCount,
+    COUNT(CASE
+        WHEN status = 'Complaint' THEN 1
+    END) AS ComplaintCount
+FROM
+    email_logs;
+`, { type: QueryTypes.SELECT });
+            return res.status(200).send(dispatcher(res, result, 'success'));
+        } catch (error) {
+            next(error);
+        }
+
     }
 } 
