05506e40dc7282dd09218d50d1ee970f3f2482c6
Ramant <ramant@medhatech.in>
2025-04-28 11:14:05 +0530

    update on report files

--- src/validations/report_files.validations.ts
+++ src/validations/report_files.validations.ts
@@ -2,9 +2,10 @@ import Joi from 'joi';
 import { constents } from '../configs/constents.config';
 
 export const report_fileSchema = Joi.object().keys({
-    report_name:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
-    filters:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
-    columns:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN)
+    report_name: Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
+    filters: Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
+    columns: Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
+    report_type: Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN)
 });
 
 export const report_fileUpdateSchema = Joi.object().keys({
