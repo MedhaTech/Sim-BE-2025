24859bb4557a8a16203520df76b0836a736827cd
Ramant <ramant@medhatech.in>
2025-04-28 11:00:00 +0530

    report files new table

---
+++ src/validations/report_files.validations.ts
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+import { constents } from '../configs/constents.config';
+
+export const report_fileSchema = Joi.object().keys({
+    report_name:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
+    filters:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN),
+    columns:Joi.string().required().regex(constents.ALPHA_NUMERIC_PLUS_PATTERN)
+});
+
+export const report_fileUpdateSchema = Joi.object().keys({
+    status: Joi.string().valid('ACTIVE', 'INACTIVE')
+});
+
